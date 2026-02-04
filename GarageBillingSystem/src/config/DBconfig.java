22770322ff0d30cd034c608bdcee23f228c300f5
amanraooo <ak9070777@gmail.com>
2026-02-04 17:13:31 +0530

    dbconfig

--- GarageBillingSystem/src/config/DBconfig.java
+++ GarageBillingSystem/src/config/DBconfig.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBconfig {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/grage";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/garage";
     private static final String USER = "root";
     private static final String PASS = "root";
 
