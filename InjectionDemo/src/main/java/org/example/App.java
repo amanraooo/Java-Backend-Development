a44e87348b434eb6834d5980771f5d4c234c8a8b
Aman Gaade <ak9070777@gmail.com>
2026-02-09 14:47:42 +0530

    injection

--- InjectionDemo/src/main/java/org/example/App.java
+++ InjectionDemo/src/main/java/org/example/App.java
@@ -1,13 +1,14 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // manual dependency injection: is not required  because it is implicitly handled by spring
+//        Student s = new Student();
+//        Course course = new DSA();
+//        s.setCourse(course);
+//        s.study();
+
+        Student s = new Student(new JavaFullStack());
+        s.study();
     }
 }
