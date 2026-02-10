a1ccd7bb875c08ff3df8d69d67c6b9b6edfef5b7
Aman Gaade <ak9070777@gmail.com>
2026-02-10 16:33:04 +0530

    springBoot-P01

---
+++ SpringBoot/BootP01/BootP01/src/main/java/org/cfs/BootP01/BootP01Application.java
@@ -0,0 +1,13 @@
+package org.cfs.BootP01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootP01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootP01Application.class, args);
+	}
+
+}
