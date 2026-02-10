2f3ae65b073cef2f76795ea14923872280d0995f
Aman Gaade <ak9070777@gmail.com>
2026-02-10 14:30:21 +0530

    springCore-P02

--- SpringCore-P02/src/main/java/org/cfs/App.java
+++ SpringCore-P02/src/main/java/org/cfs/App.java
@@ -1,13 +1,19 @@
 package org.cfs;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("----------------BeanFactory Started------------------");
+        //BeanFactory factory = new XmlBeanFactory(new ClassPathResource("your-beans.xml"));
+        ApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
+        System.out.println("----------------Bean file loaded------------------");
+
+        System.out.println("sending request.....");
+        System.out.println("--------------first call-----------------");
+        Car car1=context.getBean(Car.class);
+
+        car1.drive();
     }
 }
