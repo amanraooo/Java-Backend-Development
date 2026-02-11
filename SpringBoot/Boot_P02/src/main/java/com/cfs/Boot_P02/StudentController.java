6e61a8074281eaafb7c36929e0289d9e30842260
Aman Gaade <ak9070777@gmail.com>
2026-02-11 17:27:56 +0530

    springBoot-P02

---
+++ SpringBoot/Boot_P02/src/main/java/com/cfs/Boot_P02/StudentController.java
@@ -0,0 +1,13 @@
+package com.cfs.Boot_P02;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return  "You are welcome..";
+	}
+}
