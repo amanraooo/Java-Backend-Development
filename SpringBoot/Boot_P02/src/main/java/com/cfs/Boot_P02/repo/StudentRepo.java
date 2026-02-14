1de5406a769f12213a35fee1be4a81b1d151b401
Aman Gaade <ak9070777@gmail.com>
2026-02-14 12:30:13 +0530

    repo

---
+++ SpringBoot/Boot_P02/src/main/java/com/cfs/Boot_P02/repo/StudentRepo.java
@@ -0,0 +1,11 @@
+package com.cfs.Boot_P02.repo;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepo {
+
+	public String getStudentData(){
+		return "This is student  data";
+	}
+}
