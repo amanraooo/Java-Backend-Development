a677ea4ff35a1279577c2bb17f71c4a5b9266d4c
Aman Gaade <ak9070777@gmail.com>
2026-02-15 18:44:02 +0530

    JPA P01

---
+++ SpringBoot/JPA_P01/src/test/java/com/cfs/JPA_P01/JpaP01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfs.JPA_P01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaP01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
