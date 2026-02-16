a9a1404edfebe3c93232c45b111bfec9c65f9a71
Aman Gaade <ak9070777@gmail.com>
2026-02-16 17:53:03 +0530

    course controller

---
+++ SpringBoot/JPA_P02/src/main/java/com/cfs/JPA_P02/entity/Course.java
@@ -0,0 +1,22 @@
+package com.cfs.JPA_P02.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+@Table(name="courses")
+public class Course {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+
+	@ManyToMany(mappedBy = "courses")
+	@JsonIgnoreProperties("courses")
+	private Set<Student> student=new HashSet<>();
+}
