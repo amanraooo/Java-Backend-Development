ccb69239d4c5cbd7f14183b06754b7bfc0c982be
Aman Gaade <ak9070777@gmail.com>
2026-02-16 11:46:35 +0530

    entity

--- SpringBoot/JPA_P01/src/main/java/com/cfs/JPA_P01/entity/Student.java
+++ SpringBoot/JPA_P01/src/main/java/com/cfs/JPA_P01/entity/Student.java
@@ -1,4 +1,44 @@
 package com.cfs.JPA_P01.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Student {
+
+	@Id
+	private Long id;
+	private String name;
+	private String email;
+
+	public Student(){}
+
+	public Student(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
