f043ec433bb4ebfd9bcbc9f2888dc8c0cccf7dff
Aman Gaade <ak9070777@gmail.com>
2026-02-16 12:00:45 +0530

    controller

--- SpringBoot/JPA_P01/src/main/java/com/cfs/JPA_P01/controller/StudentController.java
+++ SpringBoot/JPA_P01/src/main/java/com/cfs/JPA_P01/controller/StudentController.java
@@ -1,4 +1,28 @@
 package com.cfs.JPA_P01.controller;
 
+import com.cfs.JPA_P01.entity.Student;
+import com.cfs.JPA_P01.repo.StudentRepo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
 public class StudentController {
+
+	private final StudentRepo studentRepo;
+
+	public StudentController(StudentRepo studentRepo){
+		this.studentRepo = studentRepo;
+	}
+
+	@PostMapping
+	public Student createStudent(@RequestBody Student student){
+		return studentRepo.save(student);
+	}
+
+	@GetMapping
+	public List<Student> getAllStudent(){
+		return studentRepo.findAll();
+	}
 }
