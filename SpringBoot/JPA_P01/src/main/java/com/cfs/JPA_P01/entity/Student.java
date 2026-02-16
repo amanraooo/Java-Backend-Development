305d83babbf2ef860fc99f5c50c40f0ac9a29149
Aman Gaade <ak9070777@gmail.com>
2026-02-16 11:49:13 +0530

    entity

--- SpringBoot/JPA_P01/src/main/java/com/cfs/JPA_P01/entity/Student.java
+++ SpringBoot/JPA_P01/src/main/java/com/cfs/JPA_P01/entity/Student.java
@@ -1,12 +1,15 @@
 package com.cfs.JPA_P01.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Student {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String name;
 	private String email;
