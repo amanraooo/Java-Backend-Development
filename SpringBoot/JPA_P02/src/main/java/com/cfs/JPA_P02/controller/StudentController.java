44c698c3449c7121fae670511d6089530aaf44fa
Aman Gaade <ak9070777@gmail.com>
2026-02-16 16:41:07 +0530

    student controller

---
+++ SpringBoot/JPA_P02/src/main/java/com/cfs/JPA_P02/controller/StudentController.java
@@ -0,0 +1,9 @@
+package com.cfs.JPA_P02.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class StudentController {
+}
