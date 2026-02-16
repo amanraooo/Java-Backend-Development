415240c72a97d101518f763f39cb0ecddfd52a0a
Aman Gaade <ak9070777@gmail.com>
2026-02-16 16:29:55 +0530

    entity

--- SpringBoot/JPA_P02/src/main/java/com/cfs/JPA_P02/entity/Student.java
+++ SpringBoot/JPA_P02/src/main/java/com/cfs/JPA_P02/entity/Student.java
@@ -1,9 +1,6 @@
 package com.cfs.JPA_P02.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Student {
@@ -12,4 +9,31 @@ public class Student {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+
+	@OneToOne(mappedBy = "student", cascade = CascadeType.All)
+	private Laptop laptop;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Laptop getLaptop() {
+		return laptop;
+	}
+
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
 }
