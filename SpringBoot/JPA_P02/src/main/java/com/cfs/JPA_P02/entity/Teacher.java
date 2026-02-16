92ff004bf34f3c415d957f530621fdf4fc8ec945
Aman Gaade <ak9070777@gmail.com>
2026-02-16 17:28:10 +0530

    student controller

--- SpringBoot/JPA_P02/src/main/java/com/cfs/JPA_P02/entity/Teacher.java
+++ SpringBoot/JPA_P02/src/main/java/com/cfs/JPA_P02/entity/Teacher.java
@@ -1,38 +1,19 @@
 package com.cfs.JPA_P02.entity;
 
+import com.cfs.JPA_P02.entity.Student;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Teacher {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String name;
 
-	@OneToMany(mappedBy = "teacher", cascade = CascadeType.ALL)
-	private Student student;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Student getStudent() {
-		return student;
-	}
-
-	public void setStudent(Student student) {
-		this.student = student;
-	}
+	@OneToMany(mappedBy = "teacher")
+	private List<Student> student=new ArrayList<>();
 }
