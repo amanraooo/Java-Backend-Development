474fa18a6acaef053d3066a849534f9f7734594f
Aman Gaade <ak9070777@gmail.com>
2026-02-17 16:34:25 +0530

    weather service

--- WeatherApp/src/main/java/com/cfs/WeatherApp/service/WeatherService.java
+++ WeatherApp/src/main/java/com/cfs/WeatherApp/service/WeatherService.java
@@ -1,4 +1,28 @@
 package com.cfs.WeatherApp.service;
+import com.cfs.WeatherApp.dto.Root;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@Service
 public class WeatherService {
+
+	@Value("${weather.api.key}")
+	private String apiKey;
+
+	@Value("${weather.api.url}")
+	private String apiUrl;
+
+	private RestTemplate template =  new RestTemplate();
+
+
+	public String test(){
+		return "good";
+	}
+
+	public Root getData(String city){
+		String url = apiUrl + "?key="+apiKey+"&q="+city;
+		Root response = template.getForObject(url,Root.class);
+		return response;
+	}
 }
