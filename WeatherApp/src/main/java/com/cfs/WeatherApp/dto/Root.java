1a0978058c30f0037f7141afa1127591f343ae3c
Aman Gaade <ak9070777@gmail.com>
2026-02-17 13:54:39 +0530

    root

--- WeatherApp/src/main/java/com/cfs/WeatherApp/dto/Root.java
+++ WeatherApp/src/main/java/com/cfs/WeatherApp/dto/Root.java
@@ -3,4 +3,28 @@ package com.cfs.WeatherApp.dto;
 public class Root{
 	public Location location;
 	public Current current;
+
+	public Root(Location location, Current current) {
+		this.location = location;
+		this.current = current;
+	}
+
+	public Root() {
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public Current getCurrent() {
+		return current;
+	}
+
+	public void setCurrent(Current current) {
+		this.current = current;
+	}
 }
