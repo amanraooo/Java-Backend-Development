32df771c66f06264e5d738aaf36953ee3ed4aeb5
Aman Gaade <ak9070777@gmail.com>
2026-02-19 13:21:14 +0530

    weather service

--- WeatherApp/src/main/java/com/cfs/WeatherApp/service/WeatherService.java
+++ WeatherApp/src/main/java/com/cfs/WeatherApp/service/WeatherService.java
@@ -1,9 +1,11 @@
 package com.cfs.WeatherApp.service;
-import com.cfs.WeatherApp.dto.Root;
-import com.cfs.WeatherApp.dto.WeatherResponse;
+import com.cfs.WeatherApp.dto.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class WeatherService {
@@ -13,6 +15,9 @@ public class WeatherService {
 
 	@Value("${weather.api.url}")
 	private String apiUrl;
+
+	@Value("${weather.api.forecast.url}")
+	private String apiForecastUrl;
 
 	private RestTemplate template =  new RestTemplate();
 
@@ -34,5 +39,32 @@ public class WeatherService {
 		weatherResponse.setTemperature(response.getCurrent().getTemp_c());
 
 		return weatherResponse;
+	}
+
+	public WeatherForeCast getForeCast(String city, int days)
+	{
+		WeatherForeCast weatherForeCast=new WeatherForeCast();
+		WeatherResponse weatherResponse = getData(city);
+		WeatherForeCast response = new WeatherForeCast();
+		response.setWeatherResponse(weatherResponse);
+
+		List<DayTemp> dayList = new ArrayList<>();
+		String url = apiForecastUrl +"?key="+apiKey+"&q="+city+"&days="+days;
+		Root apiResponse = template.getForObject(url, Root.class);
+		Forecast forecast = apiResponse.getForecast();
+		ArrayList<Forecastday> forecastday = forecast.getForecastday();
+		for(Forecastday rs: forecastday)
+		{
+			DayTemp d=new DayTemp();
+			d.setDate(rs.getDate());
+			d.setMinTemp(rs.getDay().mintemp_c);
+			d.setAvgTemp(rs.getDay().avgtemp_c);
+			d.setMaxTemp(rs.getDay().maxtemp_c);
+			dayList.add(d);
+		}
+
+		response.setDayTemp(dayList);
+		return response;
+
 	}
 }
