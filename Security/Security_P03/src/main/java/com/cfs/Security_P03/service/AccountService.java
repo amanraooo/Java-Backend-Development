c438c62c51f191734cbf3b8ed89fe83a105f0c8a
Aman Gaade <ak9070777@gmail.com>
2026-02-28 15:42:50 +0530

    service

--- Security/Security_P03/src/main/java/com/cfs/Security_P03/service/AccountService.java
+++ Security/Security_P03/src/main/java/com/cfs/Security_P03/service/AccountService.java
@@ -1,14 +1,17 @@
 package com.cfs.Security_P03.service;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
 
+	@PreAuthorize("hasRole('USER')")
 	public String getBalance(){
 		return "Your balance is = 8000";
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	public String closeAccount(){
 		return "Account closed";
 	}
