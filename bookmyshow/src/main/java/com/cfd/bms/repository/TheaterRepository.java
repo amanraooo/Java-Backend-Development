5b10a608fba7f565d2e335a4871e2df86b7cfbeb
Aman Gaade <ak9070777@gmail.com>
2026-03-05 13:12:52 +0530

    TheaterRepository

---
+++ bookmyshow/src/main/java/com/cfd/bms/repository/TheaterRepository.java
@@ -0,0 +1,14 @@
+package com.cfd.bms.repository;
+
+import com.cfd.bms.model.Theater;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TheaterRepository extends JpaRepository<Theater,Long>
+{
+	List<Theater> findByCity(String city);
+
+}
