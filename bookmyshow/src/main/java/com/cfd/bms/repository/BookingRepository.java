1684d23ead15952acf536ffecd356da0c63c9d41
Aman Gaade <ak9070777@gmail.com>
2026-03-05 12:55:22 +0530

    Booking repo

--- bookmyshow/src/main/java/com/cfd/bms/repository/BookingRepository.java
+++ bookmyshow/src/main/java/com/cfd/bms/repository/BookingRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface BookingRepository extends JpaRepository<BookingRepository, Long> {
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 
 	List<Booking> findByUserId(Long userId);
 
