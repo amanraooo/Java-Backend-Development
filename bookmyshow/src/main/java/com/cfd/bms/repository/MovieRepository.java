b2a4268cf020969a1ccfbd6c33073c68bb45a591
Aman Gaade <ak9070777@gmail.com>
2026-03-05 12:54:57 +0530

    movie repo

--- bookmyshow/src/main/java/com/cfd/bms/repository/MovieRepository.java
+++ bookmyshow/src/main/java/com/cfd/bms/repository/MovieRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface MovieRepository extends JpaRepository<MovieRepository, Long> {
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 
 	List<Movie> findByLanguage(String language);
 
