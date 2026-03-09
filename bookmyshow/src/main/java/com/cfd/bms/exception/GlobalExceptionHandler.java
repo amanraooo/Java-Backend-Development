5c5b7edd43a2d84299e69264fcd54eb6c8e509e4
Aman Gaade <ak9070777@gmail.com>
2026-03-09 13:10:57 +0530

    GlobalExceptionHandler

--- bookmyshow/src/main/java/com/cfd/bms/exception/GlobalExceptionHandler.java
+++ bookmyshow/src/main/java/com/cfd/bms/exception/GlobalExceptionHandler.java
@@ -1,4 +1,20 @@
 package com.cfd.bms.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import java.util.Date;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+	@ExceptionHandler(ResourceNotFoundException.class)
+
+	public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request){
+		ErrorResponse errorDetails =new ErrorResponse(new Date(), HttpStatus.NOT_FOUND.value(),
+				"Not Found",ex.getMessage(),request.getDescription(false));
+
+		return  new ResponseEntity<>(errorDetails,HttpStatus.NOT_FOUND);
+	}
 }
