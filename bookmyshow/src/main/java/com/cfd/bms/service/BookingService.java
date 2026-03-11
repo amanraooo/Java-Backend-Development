1f9a03b26557bd462fa5fd1b1ce874132a36c7ee
Aman Gaade <ak9070777@gmail.com>
2026-03-11 13:26:21 +0530

    Booking service

--- bookmyshow/src/main/java/com/cfd/bms/service/BookingService.java
+++ bookmyshow/src/main/java/com/cfd/bms/service/BookingService.java
@@ -2,13 +2,28 @@ package com.cfd.bms.service;
 
 import com.cfd.bms.dto.BookingDto;
 import com.cfd.bms.dto.BookingRequestDto;
-import jakarta.validation.Valid;
+import com.cfd.bms.exception.ResourceNotFoundException;
+import com.cfd.bms.repository.ShowRepository;
+import com.cfd.bms.repository.UserRepository;
+import com.cfd.bms.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookingService {
 
-	public  BookingDto createBooking(@Valid BookingRequestDto bookingRequest) {
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private ShowRepository showRepository;
+
+	public  BookingDto createBooking(BookingRequestDto bookingRequest) {
+		User user = userRepository.findById(bookingRequest.getUserId())
+				.orElseThrow(()-> new ResourceNotFoundException("User Not Found"));
+
+		Show show = showRepository.findById(bookingRequest.getShowId())
+				.orElseThrow(()-> new ResourceNotFoundException(("Show not found")));
 
 	}
 }
