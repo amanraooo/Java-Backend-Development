71ef2b951cfeb85eed66dace0a4cb838663aa030
Aman Gaade <ak9070777@gmail.com>
2026-03-14 18:15:36 +0530

    Movie service

--- bookmyshow/src/main/java/com/cfd/bms/service/MovieService.java
+++ bookmyshow/src/main/java/com/cfd/bms/service/MovieService.java
@@ -1,12 +1,40 @@
 package com.cfd.bms.service;
 
 import com.cfd.bms.dto.MovieDto;
+import com.cfd.bms.exception.ResourceNotFoundException;
 import com.cfd.bms.model.Movie;
+import com.cfd.bms.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MovieService {
 
+	@Autowired
+	private MovieRepository movieRepository;
+
+	public MovieDto createMovie(MovieDto movieDto){
+		Movie movie = mapToEntity(movieDto);
+		Movie saveMovie = movieRepository.save(movie);
+		return mapToDto(saveMovie);
+	}
+
+	public MovieDto getMovieById(Long id){
+		Movie movie = movieRepository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Movie not Found"));
+		return mapToDto(movie);
+	}
+
+	public List<MovieDto> getAllMovies(){
+		List<Movie> movies = movieRepository.findAll();
+		return movies.stream()
+				.map(this::mapToDto)
+				.collect(Collectors.toList());
+
+	}
 	private MovieDto mapToDto(Movie movie)
 	{
 		MovieDto movieDto=new MovieDto();
