53fcdbd617b8acd45b463b1c672a4d1ac185cf44
Aman Gaade <ak9070777@gmail.com>
2026-03-17 16:59:38 +0530

    Show service

--- bookmyshow/src/main/java/com/cfd/bms/service/ShowService.java
+++ bookmyshow/src/main/java/com/cfd/bms/service/ShowService.java
@@ -1,6 +1,14 @@
 package com.cfd.bms.service;
 
+import com.cfd.bms.dto.ShowDto;
+import com.cfd.bms.exception.ResourceNotFoundException;
+import com.cfd.bms.model.Movie;
+import com.cfd.bms.model.Screen;
+import com.cfd.bms.model.Show;
 import com.cfd.bms.repository.MovieRepository;
+import com.cfd.bms.repository.ScreenRepository;
+import com.cfd.bms.repository.ShowRepository;
+import com.cfd.bms.repository.ShowSeatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class ShowService {
@@ -8,4 +16,33 @@ public class ShowService {
 	@Autowired
 	private MovieRepository movieRepository;
 
+	@Autowired
+	private ScreenRepository screenRepository;
+
+	@Autowired
+	private ShowRepository showRepository;
+
+	@Autowired
+	private ShowSeatRepository showSeatRepository;
+
+
+	public ShowDto createShow(ShowDto showDto){
+		Show show = new Show();
+
+		Movie movie = movieRepository.findById(ShowDto.getMovie().getId())
+				.orElseThrow(()-> new ResourceNotFoundException("Movie Not Found"));
+
+		Screen screen = screenRepository.findById(ShowDto.getScreen().getId())
+				.orElseThrow(()-> new ResourceNotFoundException("Screen Not Found"));
+
+		show.setMovie(movie);
+		show.setScreen(screen);
+		show.setStartTime(showDto.getStartTime());
+		show.setEndTime(showDto.getEndTime());
+
+		Show saveShow = showRepository.save(show);
+
+
+
+	}
 }
