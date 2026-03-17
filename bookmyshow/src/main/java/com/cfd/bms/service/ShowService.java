86736c781c7f594ab4889772b4323e838cf43212
Aman Gaade <ak9070777@gmail.com>
2026-03-17 16:46:50 +0530

    Show service

---
+++ bookmyshow/src/main/java/com/cfd/bms/service/ShowService.java
@@ -0,0 +1,11 @@
+package com.cfd.bms.service;
+
+import com.cfd.bms.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ShowService {
+
+	@Autowired
+	private MovieRepository movieRepository;
+
+}
