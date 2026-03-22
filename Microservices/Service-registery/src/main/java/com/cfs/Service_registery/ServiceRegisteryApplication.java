2889719fd24cb703e4c53533ac12900b96b618f7
Aman Gaade <ak9070777@gmail.com>
2026-03-22 21:28:05 +0530

    Service Registery

---
+++ Microservices/Service-registery/src/main/java/com/cfs/Service_registery/ServiceRegisteryApplication.java
@@ -0,0 +1,13 @@
+package com.cfs.Service_registery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegisteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegisteryApplication.class, args);
+	}
+
+}
