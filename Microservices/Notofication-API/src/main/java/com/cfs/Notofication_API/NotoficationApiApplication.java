331289779709fb2215562c895f546b9398931eda
Aman Gaade <ak9070777@gmail.com>
2026-03-31 15:53:38 +0530

    Notification api app

--- Microservices/Notofication-API/src/main/java/com/cfs/Notofication_API/NotoficationApiApplication.java
+++ Microservices/Notofication-API/src/main/java/com/cfs/Notofication_API/NotoficationApiApplication.java
@@ -2,8 +2,12 @@ package com.cfs.Notofication_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class NotoficationApiApplication {
 
 	public static void main(String[] args) {
