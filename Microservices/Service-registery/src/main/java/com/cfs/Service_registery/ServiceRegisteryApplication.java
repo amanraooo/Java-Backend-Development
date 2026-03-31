56db7122ccfadf48d410964e7b0ed78918e6023c
Aman Gaade <ak9070777@gmail.com>
2026-03-31 13:08:26 +0530

    Service Registery

--- Microservices/Service-registery/src/main/java/com/cfs/Service_registery/ServiceRegisteryApplication.java
+++ Microservices/Service-registery/src/main/java/com/cfs/Service_registery/ServiceRegisteryApplication.java
@@ -2,7 +2,9 @@ package com.cfs.Service_registery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegisteryApplication {
 
