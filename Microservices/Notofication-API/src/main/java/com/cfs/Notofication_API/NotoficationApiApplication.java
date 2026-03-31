9fddc91e3ca578b8b82e5e14610f04070b3514a6
Aman Gaade <ak9070777@gmail.com>
2026-03-31 15:39:41 +0530

    Notification api

---
+++ Microservices/Notofication-API/src/main/java/com/cfs/Notofication_API/NotoficationApiApplication.java
@@ -0,0 +1,13 @@
+package com.cfs.Notofication_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotoficationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotoficationApiApplication.class, args);
+	}
+
+}
---
+++ Microservices/Notofication-API/src/test/java/com/cfs/Notofication_API/NotoficationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfs.Notofication_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotoficationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
