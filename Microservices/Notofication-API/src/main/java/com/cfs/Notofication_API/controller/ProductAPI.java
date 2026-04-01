a309d64a7d43b55ddf08986a33b8c7be7bee2f3b
Aman Gaade <ak9070777@gmail.com>
2026-04-01 16:49:27 +0530

    product api

--- Microservices/Notofication-API/src/main/java/com/cfs/Notofication_API/controller/ProductAPI.java
+++ Microservices/Notofication-API/src/main/java/com/cfs/Notofication_API/controller/ProductAPI.java
@@ -1,4 +1,10 @@
 package com.cfs.Notofication_API.controller;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "PRODUCT-SERVICE")
 public interface ProductAPI {
+	@GetMapping("/place")
+	public String invokeProductAPi();
 }
