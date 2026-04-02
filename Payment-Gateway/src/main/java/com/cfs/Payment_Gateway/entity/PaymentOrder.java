59dc3d0d73eb01c86897e4ac5b2c6befb43d98bc
Aman Gaade <ak9070777@gmail.com>
2026-04-02 15:16:45 +0530

    payment order entity

---
+++ Payment-Gateway/src/main/java/com/cfs/Payment_Gateway/entity/PaymentOrder.java
@@ -0,0 +1,9 @@
+package com.cfs.Payment_Gateway.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="payment_order")
+public class PaymentOrder {
+}
