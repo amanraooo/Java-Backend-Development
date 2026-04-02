60dd5d6567c6960c0cb2168fea5aeca319238abb
Aman Gaade <ak9070777@gmail.com>
2026-04-02 16:52:17 +0530

    PaymentController

--- Payment-Gateway/src/main/java/com/cfs/Payment_Gateway/controller/PaymentController.java
+++ Payment-Gateway/src/main/java/com/cfs/Payment_Gateway/controller/PaymentController.java
@@ -5,12 +5,11 @@ import com.cfs.Payment_Gateway.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
+
 @RequestMapping("api/payment")
 public class PaymentController {
 
