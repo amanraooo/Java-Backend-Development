56b67d493adff84705aa9e087968a1249478f62f
Aman Gaade <ak9070777@gmail.com>
2026-04-02 16:22:12 +0530

    PaymentController

---
+++ Payment-Gateway/src/main/java/com/cfs/Payment_Gateway/controller/PaymentController.java
@@ -0,0 +1,10 @@
+package com.cfs.Payment_Gateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class PaymentController {
+
+}
