abc86009ab06584cf1768b6fb6fd22fdaf691939
Aman Gaade <ak9070777@gmail.com>
2026-04-02 16:55:14 +0530

    paymentService

--- Payment-Gateway/src/main/java/com/cfs/Payment_Gateway/controller/PaymentController.java
+++ Payment-Gateway/src/main/java/com/cfs/Payment_Gateway/controller/PaymentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin
 
-@RequestMapping("api/payment")
+@RequestMapping("/api/payment")
 public class PaymentController {
 
 	@Autowired
