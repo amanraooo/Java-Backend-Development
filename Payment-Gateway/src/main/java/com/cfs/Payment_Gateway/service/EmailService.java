4636ede1dcb7a759f0c54d3f89109837c37ba1c2
Aman Gaade <ak9070777@gmail.com>
2026-04-03 17:14:00 +0530

    email service

---
+++ Payment-Gateway/src/main/java/com/cfs/Payment_Gateway/service/EmailService.java
@@ -0,0 +1,8 @@
+package com.cfs.Payment_Gateway.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EmailService {
+	@Autowired
+	private JavaMailSender javaMailSender;
+}
