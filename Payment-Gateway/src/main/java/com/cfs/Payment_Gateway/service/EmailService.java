d42e9eea72e9bdeaaa7822bd4e982abb240b3a77
Aman Gaade <ak9070777@gmail.com>
2026-04-03 17:21:04 +0530

    email service

--- Payment-Gateway/src/main/java/com/cfs/Payment_Gateway/service/EmailService.java
+++ Payment-Gateway/src/main/java/com/cfs/Payment_Gateway/service/EmailService.java
@@ -5,4 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class EmailService {
 	@Autowired
 	private JavaMailSender javaMailSender;
+
+	public void sendEmail(String toEmail, String name , String course , double amount){
+		SimpleMailMessage mailMessage = SimpleMailMessage();
+		mailMessage.setTo(toEmail);
+		mailMessage.setSubject("Payment Successful "+course);
+		mailMessage.setText("Hi "+name+", \n\n"+
+				"Thank you for enrolling in "+course+" .\n\n"+
+				"We  are looking forward to see you in live class"
+				);
+		javaMailSender.send(mailMessage);
+	}
 }
