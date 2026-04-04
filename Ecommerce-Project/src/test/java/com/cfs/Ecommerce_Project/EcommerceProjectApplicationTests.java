fde48f81bc2cd16012bbf0d101a413e58a7c2811
Aman Gaade <ak9070777@gmail.com>
2026-04-04 17:08:27 +0530

    e-comm project

---
+++ Ecommerce-Project/src/test/java/com/cfs/Ecommerce_Project/EcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfs.Ecommerce_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
