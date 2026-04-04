92a95f5d5846b086dd8f3a854a627e08772db4b0
Aman Gaade <ak9070777@gmail.com>
2026-04-04 17:36:55 +0530

    orders  model

--- Ecommerce-Project/src/main/java/com/cfs/Ecommerce_Project/model/Orders.java
+++ Ecommerce-Project/src/main/java/com/cfs/Ecommerce_Project/model/Orders.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Orders {
@@ -18,4 +19,54 @@ public class Orders {
 	private double totalAmounnt;
 	private String status;
 	private Date orderDate;
+
+	private List<OrderItem> orderItem;
+
+	public List<OrderItem> getOrderItem() {
+		return orderItem;
+	}
+
+	public void setOrderItem(List<OrderItem> orderItem) {
+		this.orderItem = orderItem;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public double getTotalAmounnt() {
+		return totalAmounnt;
+	}
+
+	public void setTotalAmounnt(double totalAmounnt) {
+		this.totalAmounnt = totalAmounnt;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
