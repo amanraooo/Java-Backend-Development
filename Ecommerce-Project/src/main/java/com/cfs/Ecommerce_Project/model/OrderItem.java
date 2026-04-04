a292811e8fe288b1509acc85fbf2c42911d5651b
Aman Gaade <ak9070777@gmail.com>
2026-04-04 17:43:07 +0530

    OrderItem  model

--- Ecommerce-Project/src/main/java/com/cfs/Ecommerce_Project/model/OrderItem.java
+++ Ecommerce-Project/src/main/java/com/cfs/Ecommerce_Project/model/OrderItem.java
@@ -1,4 +1,18 @@
 package com.cfs.Ecommerce_Project.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class OrderItem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id ;
+
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Orders orders;
+
+
+
 }
