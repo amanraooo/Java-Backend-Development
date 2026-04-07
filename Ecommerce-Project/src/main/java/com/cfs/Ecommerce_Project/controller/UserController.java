56689bc014127fa0b7a387acc0e7ffb655bc1eeb
Aman Gaade <ak9070777@gmail.com>
2026-04-07 19:07:46 +0530

    UserController

--- Ecommerce-Project/src/main/java/com/cfs/Ecommerce_Project/controller/UserController.java
+++ Ecommerce-Project/src/main/java/com/cfs/Ecommerce_Project/controller/UserController.java
@@ -1,5 +1,31 @@
 package com.cfs.Ecommerce_Project.controller;
 
-public class UserController {
+import com.cfs.Ecommerce_Project.model.User;
+import com.cfs.Ecommerce_Project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin("*")
+public class UserController {
+	@Autowired
+	private UserService userService;
+
+	@PostMapping("/register")
+	public User registerUser(@RequestBody User user) {
+		return userService.registerUser(user);
+	}
+
+	@PostMapping("/login")
+	public User loginUser(@RequestBody User user) {
+		return userService.loginUser(user.getEmail(), user.getPassword());
+	}
+
+	@GetMapping
+	public List<User> getAllUsers() {
+		return userService.getAllUsers();
+	}
 }
