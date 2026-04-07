fbe7b303a6e3f59d1dc92232d2c3f7910a793d76
Aman Gaade <ak9070777@gmail.com>
2026-04-07 13:54:37 +0530

    OrderDto

--- Ecommerce-Project/src/main/java/com/cfs/Ecommerce_Project/dto/OrderDto.java
+++ Ecommerce-Project/src/main/java/com/cfs/Ecommerce_Project/dto/OrderDto.java
@@ -19,4 +19,69 @@ public class OrderDto {
 
 	private List<OrderItemDto> orderItems;
 
+	public OrderDto(Long id, String userName, String email, double totalAmounnt, String status, Date orderDate, List<OrderItemDto> orderItems) {
+		this.id = id;
+		this.userName = userName;
+		this.email = email;
+		this.totalAmounnt = totalAmounnt;
+		this.status = status;
+		this.orderDate = orderDate;
+		this.orderItems = orderItems;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public double getTotalAmounnt() {
+		return totalAmounnt;
+	}
+
+	public void setTotalAmounnt(double totalAmounnt) {
+		this.totalAmounnt = totalAmounnt;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public List<OrderItemDto> getOrderItems() {
+		return orderItems;
+	}
+
+	public void setOrderItems(List<OrderItemDto> orderItems) {
+		this.orderItems = orderItems;
+	}
 }
