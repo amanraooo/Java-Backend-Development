6b31277d5a7c6fa07d37d4ac0f3cdb8b72b37ee8
Aman Gaade <ak9070777@gmail.com>
2026-04-07 17:16:51 +0530

    ProductService

--- Ecommerce-Project/src/main/java/com/cfs/Ecommerce_Project/service/ProductService.java
+++ Ecommerce-Project/src/main/java/com/cfs/Ecommerce_Project/service/ProductService.java
@@ -1,4 +1,30 @@
 package com.cfs.Ecommerce_Project.service;
 
+import com.cfs.Ecommerce_Project.model.Product;
+import com.cfs.Ecommerce_Project.repo.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+	@Autowired
+	ProductRepository productRepository;
+
+	public List<Product> getAllProducts() {
+		return productRepository.findAll();
+	}
+
+	public Product getProductById(Long id) {
+		return productRepository.findById(id).orElse(null);
+	}
+
+	public Product addProduct(Product product) {
+		return productRepository.save(product);
+	}
+
+	public void deleteProduct(Long id) {
+		productRepository.deleteById(id);
+	}
 }
