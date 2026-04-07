50fd1aa611b2aa77f1fabc3693309fefbea10b19
Aman Gaade <ak9070777@gmail.com>
2026-04-07 17:24:10 +0530

    UserService

--- Ecommerce-Project/src/main/java/com/cfs/Ecommerce_Project/service/UserService.java
+++ Ecommerce-Project/src/main/java/com/cfs/Ecommerce_Project/service/UserService.java
@@ -1,4 +1,35 @@
 package com.cfs.Ecommerce_Project.service;
 
+import com.cfs.Ecommerce_Project.model.User;
+import com.cfs.Ecommerce_Project.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public User registerUser(User user){
+		User newUser = userRepository.save(user);
+		System.out.println("User added");
+		return newUser;
+	}
+
+	public User loginUser(String email,String password){
+		User user = userRepository.findByEmail(email);
+		if(user!=null && user.getPassword().equals(password)){
+			return user;
+		}
+		return null;
+	}
+
+	public List<User> getAllUsers(){
+		return userRepository.findAll();
+	}
+
+
 }
