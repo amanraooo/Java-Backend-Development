0a6c39b331cd38fe73f9af136b04e65f11f96203
Aman Gaade <ak9070777@gmail.com>
2026-04-07 16:56:06 +0530

    OrderRepository

--- Ecommerce-Project/src/main/java/com/cfs/Ecommerce_Project/repo/OrderRepository.java
+++ Ecommerce-Project/src/main/java/com/cfs/Ecommerce_Project/repo/OrderRepository.java
@@ -1,4 +1,15 @@
 package com.cfs.Ecommerce_Project.repo;
 
-public interface OrderRepository {
+import com.cfs.Ecommerce_Project.model.Orders;
+import com.cfs.Ecommerce_Project.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Orders, Long> {
+	@Query("SELECT o from Orders o JOIN FETCH o.user")
+	List<Orders> findAllOrdersWithUsers();
+
+	List<Orders> findByUser(User user);
 }
