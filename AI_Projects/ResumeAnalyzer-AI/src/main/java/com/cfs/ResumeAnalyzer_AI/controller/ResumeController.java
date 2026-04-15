8a887136957fe18eda5f22484e33659b8b389105
Aman Gaade <ak9070777@gmail.com>
2026-04-15 16:29:04 +0530

    ResumeController

--- AI_Projects/ResumeAnalyzer-AI/src/main/java/com/cfs/ResumeAnalyzer_AI/controller/ResumeController.java
+++ AI_Projects/ResumeAnalyzer-AI/src/main/java/com/cfs/ResumeAnalyzer_AI/controller/ResumeController.java
@@ -1,4 +1,49 @@
 package com.cfs.ResumeAnalyzer_AI.controller;
 
+import org.apache.tika.Tika;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/resume")
 public class ResumeController {
+
+	private final ChatClient chatClient;
+
+	private final Tika tika = new Tika();
+
+	public ResumeController(OpenAiChatModel openAiChatModel){
+		this.chatClient = ChatClient.create(openAiChatModel);
+	}
+
+	@PostMapping("/analyzer")
+	public Map<String, Object> analyzer(@RequestParam("file")MultipartFile file) throws Exception{
+
+		String content = tika.parseToString(file.getInputStream());
+
+		String prompt = """
+				Analyze this resume text:
+				%s
+				1. Extract key skills
+				2. Rate overall resume quality (1-10)
+				3. Suggest 4 improvements
+				Reply in structured JSON format.
+				
+				""".formatted(content);
+
+		String aiResponse = chatClient.prompt()
+				.user(prompt)
+				.call()
+				.content();
+
+		return Map.of("analysis", aiResponse);
+
+	}
 }
