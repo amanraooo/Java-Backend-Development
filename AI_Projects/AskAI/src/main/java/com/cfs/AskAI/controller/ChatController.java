c25a50ae55636bff718a3b1dbb2e300077dbdeec
Aman Gaade <ak9070777@gmail.com>
2026-04-15 14:21:20 +0530

    ChatController

--- AI_Projects/AskAI/src/main/java/com/cfs/AskAI/controller/ChatController.java
+++ AI_Projects/AskAI/src/main/java/com/cfs/AskAI/controller/ChatController.java
@@ -1,7 +1,12 @@
 package com.cfs.AskAI.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/chat")
 public class ChatController {
+
+	private final ChatClient;
 }
