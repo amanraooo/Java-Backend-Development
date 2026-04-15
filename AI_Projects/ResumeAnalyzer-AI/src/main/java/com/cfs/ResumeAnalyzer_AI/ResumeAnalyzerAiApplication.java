2910e6dc6580711dfd6097ffc5199c1355df05e0
Aman Gaade <ak9070777@gmail.com>
2026-04-15 15:32:45 +0530

    ResumeAnalyzer-AI Project

---
+++ AI_Projects/ResumeAnalyzer-AI/src/main/java/com/cfs/ResumeAnalyzer_AI/ResumeAnalyzerAiApplication.java
@@ -0,0 +1,13 @@
+package com.cfs.ResumeAnalyzer_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAnalyzerAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeAnalyzerAiApplication.class, args);
+	}
+
+}
---
+++ AI_Projects/ResumeAnalyzer-AI/src/test/java/com/cfs/ResumeAnalyzer_AI/ResumeAnalyzerAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfs.ResumeAnalyzer_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAnalyzerAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
