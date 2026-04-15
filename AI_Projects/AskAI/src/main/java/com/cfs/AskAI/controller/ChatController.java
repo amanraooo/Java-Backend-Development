044cb5fda7a609b6ba37dc367e0c62af707b880f
Aman Gaade <ak9070777@gmail.com>
2026-04-15 14:30:09 +0530

    ChatController

--- AI_Projects/AskAI/src/main/java/com/cfs/AskAI/controller/ChatController.java
+++ AI_Projects/AskAI/src/main/java/com/cfs/AskAI/controller/ChatController.java
@@ -1,12 +1,31 @@
 package com.cfs.AskAI.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/chat")
+@CrossOrigin(origins="*")
 public class ChatController {
 
-	private final ChatClient;
+	private final ChatClient chatClient;
+
+	@Autowired
+	public ChatController(OpenAiChatModel openAiChatModel){
+		this.chatClient= ChatClient.create(openAiChatModel);
+	}
+
+	@PostMapping
+	public Map<String,String> chat(@RequestBody Map<String,String> body){
+		String question = body.get("question");
+		String answer = chatClient.prompt()
+				.user(question)
+				.call()
+				.content();
+		return  Map.of("amswer", answer);
+	}
 }
