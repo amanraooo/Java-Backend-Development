434b6169c68dcbf9d2a4bf5784d184753bd6352a
Aman Gaade <ak9070777@gmail.com>
2026-04-16 16:00:27 +0530

    websocket config

--- AI_Projects/ChatBot/src/main/java/com/cfs/ChatBot/config/WebSocketConfig.java
+++ AI_Projects/ChatBot/src/main/java/com/cfs/ChatBot/config/WebSocketConfig.java
@@ -1,5 +1,7 @@
 package com.cfs.ChatBot.config;
 
+import com.cfs.ChatBot.controller.GeminiWebSocketHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -9,9 +11,17 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
-	@Override
-	public class registerWebSocketHandlers(WebSocketHandlerRegistry registry){
+	private GeminiWebSocketHandler geminiWebSocketHandler;
 
+	@Autowired
+	public WebSocketConfig(GeminiWebSocketHandler geminiWebSocketHandler){
+		this.geminiWebSocketHandler= geminiWebSocketHandler;
+	}
+
+
+
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
+		registry.addHandler(geminiWebSocketHandler, "/chat").setAllowedOrigins("*");
 	}
 
 }
