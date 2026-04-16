daab730d81157d014d898daac31c4d5a20bc4ad7
Aman Gaade <ak9070777@gmail.com>
2026-04-16 16:05:40 +0530

    GeminiWebSocketHandler

--- AI_Projects/ChatBot/src/main/java/com/cfs/ChatBot/controller/GeminiWebSocketHandler.java
+++ AI_Projects/ChatBot/src/main/java/com/cfs/ChatBot/controller/GeminiWebSocketHandler.java
@@ -1,9 +1,16 @@
 package com.cfs.ChatBot.controller;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
 public class GeminiWebSocketHandler extends TextWebSocketHandler {
+
+	@Override
+	protected void handleTextMessage(WebSocketSession session , TextMessage message) {
+		System.out.println("Hello bhai");
+	}
 
 }
