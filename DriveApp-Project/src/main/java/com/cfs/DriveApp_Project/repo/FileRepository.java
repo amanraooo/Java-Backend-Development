5b1b035abb34fd71965d0195639f40a43f465d9d
Aman Gaade <ak9070777@gmail.com>
2026-04-17 09:41:19 +0530

    FileRepository

--- DriveApp-Project/src/main/java/com/cfs/DriveApp_Project/repo/FileRepository.java
+++ DriveApp-Project/src/main/java/com/cfs/DriveApp_Project/repo/FileRepository.java
@@ -1,4 +1,9 @@
 package com.cfs.DriveApp_Project.repo;
 
-public class FileRepository {
+import com.cfs.DriveApp_Project.entity.FileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface FileRepository extends JpaRepository<FileEntity,Long> {
+
 }
