95ce4f186803712fb62c46458a8d15c94af264b9
Aman Gaade <ak9070777@gmail.com>
2026-04-17 14:01:05 +0530

    FileController

--- DriveApp-Project/src/main/java/com/cfs/DriveApp_Project/controller/FileController.java
+++ DriveApp-Project/src/main/java/com/cfs/DriveApp_Project/controller/FileController.java
@@ -1,4 +1,32 @@
 package com.cfs.DriveApp_Project.controller;
 
+import com.cfs.DriveApp_Project.service.FileServiceStorage;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/files")
+@CrossOrigin(origins = "http://localhost:3000")
 public class FileController {
+
+	private final FileServiceStorage fileServiceStorage;
+
+	public FileController(FileServiceStorage fileServiceStorage) {
+		this.fileServiceStorage = fileServiceStorage;
+	}
+
+	public ResponseEntity<String> uploadFile(@RequestParam("file")MultipartFile file,
+											 @RequestParam(value = "parentFolderId", required = false) Long parentFolderId){
+		try{
+			String response = fileServiceStorage.saveFile(file,parentFolderId);
+		} catch (Exception e) {
+			return ResponseEntity.status(500).body("File upload failed");
+		}
+	}
 }
